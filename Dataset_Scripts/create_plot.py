02d61caf89d38a72f87751440670345f216ec19c
George Paterakis - From Kalouris <creaperlost@gmail.com>
2023-04-04 13:36:36 +0300

    Fix Plot

--- Dataset_Scripts/create_plot.py
+++ Dataset_Scripts/create_plot.py
@@ -448,14 +448,14 @@ def plot_average(config,plot = True,extra_data = None):
 
 data_repo = 'Jad'
 n_seeds=  3
-    #optimizers = ['HEBO_RF','GP','RS','HEBO_GP','HEBO_RF5','HEBO_RF10'] #['RF','GP','RS','HEBO_RF','HEBO_GP'] 'RF','HEBO_GP']
-    #'Sobol', 'HEBO_RF_Scipy'
-    #,'GP'
-    #,'HEBO_RF_ACQ10000','HEBO_RF_RANDOM'
-    optimizers = ['HEBO_RF','RS'] #'HEBO_RF_ACQ100','HEBO_RF_ACQ500' 
-    metrics = ['Metric','Surrogate_Time','Objective_Time','Acquisition_Time','Total_Time']
-    time_plot = True
-    double_plot = False
+#optimizers = ['HEBO_RF','GP','RS','HEBO_GP','HEBO_RF5','HEBO_RF10'] #['RF','GP','RS','HEBO_RF','HEBO_GP'] 'RF','HEBO_GP']
+#'Sobol', 'HEBO_RF_Scipy'
+#,'GP'
+#,'HEBO_RF_ACQ10000','HEBO_RF_RANDOM'
+optimizers = ['HEBO_RF','RS'] #'HEBO_RF_ACQ100','HEBO_RF_ACQ500' 
+metrics = ['Metric','Surrogate_Time','Objective_Time','Acquisition_Time','Total_Time']
+time_plot = True
+double_plot = False
 
 
 
