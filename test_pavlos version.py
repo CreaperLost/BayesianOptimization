d4456c9adad5cf88a3632a643dc4d8dc3703badc
George Paterakis - From Kalouris <creaperlost@gmail.com>
2023-05-09 16:26:59 +0300

    hm

--- test_pavlos version.py
+++ test_pavlos version.py
@@ -1,11 +1,3 @@
-from ConfigSpace import Configuration, ConfigurationSpace
-
-import numpy as np
-from smac import HyperparameterOptimizationFacade, Scenario
-from sklearn import datasets
-from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score
-from benchmarks.Group_MulltiFoldBenchmark import Group_MultiFold_Space
 import openml
 import warnings
 warnings.simplefilter("ignore", UserWarning)
@@ -22,7 +14,6 @@ from benchmarks.Group_MulltiFoldBenchmark import Group_MultiFold_Space
 from global_utilities.global_util import csv_postfix,parse_directory
 from pathlib import Path
 import numpy as np
-from smac import MultiFidelityFacade as MFFacade
 
 
 
@@ -110,74 +101,50 @@ def run_benchmark_total(optimizers_used =[],bench_config={},save=True):
                 configspace,config_dict = benchmark_.get_configuration_space()
 
                 #Get the benchmark.
-                objective_function = benchmark_.smac_objective_function
+                objective_function = benchmark_.objective_function
                 
                 #Get the objective_function per fold.
-                objective_function_per_fold = benchmark_.smac_objective_function_per_fold
+                objective_function_per_fold = benchmark_.objective_function_per_fold
 
                 print('Currently running ' + opt + ' on seed ' + str(seed) + ' dataset ' + str(task_id) )
 
-                # Scenario object specifying the optimization environment
-                scenario = Scenario(configspace,
-                                    name='Smac on dataset '+str(task_id) + 'seed ' + str(seed), 
-                                    n_trials=max_evals,
-                                    deterministic=True,seed=seed)
+                if opt == 'Random_Search':
+                    Optimization = Group_Random_Search(f=objective_function,configuration_space= configspace,n_init = n_init,max_evals= max_evals,random_seed=seed)
+                # Single k-Fold Validation.
+                elif opt == 'RF_Local':
+                    Optimization = Group_Bayesian_Optimization(f=objective_function, model='RF' ,lb= None, ub =None , configuration_space= config_dict ,\
+                    initial_design=None,n_init = n_init, max_evals = max_evals, batch_size=1 ,verbose=True,random_seed=seed,maximizer = 'Sobol_Local')
+                elif opt == 'Multi_RF_Local':
+                    Optimization = Experimental_MultiFold_Group_Bayesian_Optimization(f=objective_function_per_fold, model='RF' ,lb= None, ub =None , configuration_space= config_dict ,\
+                    initial_design=None,n_init = n_init, max_evals = max_evals, batch_size=1 ,verbose=True,random_seed=seed,maximizer = 'Sobol_Local',n_folds=10)
+                else: 
+                    print(opt)
+                    raise RuntimeError
+                """
+                elif opt == 'Multi_RF_Local':
+                    Optimization = Bayesian_Optimization_MultiFold(f=objective_function_per_fold, model='RF' ,lb= None, ub =None , configuration_space= configspace ,\
+                    initial_design=None,n_init = n_init, max_evals = max_evals, batch_size=1 ,verbose=True,random_seed=seed,maximizer = 'Sobol_Local')
+                """
+                
+                
 
-                # Use SMAC to find the best configuration/hyperparameters
-                smac = HyperparameterOptimizationFacade(scenario, objective_function,seed=seed)
-                incumbent = smac.optimize()
-                print(incumbent)
+                best_score = Optimization.run()
+                print('Total Time',np.sum(Optimization.total_time))
+                print(Optimization.inc_score,Optimization.inc_config)
 
+                #Get the evaluatinons.
+                y_evaluations = Optimization.fX
 
-                scenario = Scenario(
-                        configspace,
-                        n_trials=max_evals,  # We want to try max 5000 different trials
-                        min_budget=1,  # Use min 1 fold.
-                        max_budget=10,  # Use max 10 folds. 
-                        instances=[0,1,2,3,4,5,6,7,8,9],
-                    )
-                # Create our SMAC object and pass the scenario and the train method
-                smac = MFFacade(
-                    scenario,
-                    objective_function_per_fold,
-                    overwrite=True,
-                )
-
-                # Now we start the optimization process
-                incumbent = smac.optimize()
-                print(incumbent)
-
+                #Change this.
+                surrogate_time_evaluations = Optimization.surrogate_time
+                objective_time_evaluations= Optimization.objective_time
+                acquisition_time_evaluations = Optimization.acquisition_time
+                total_time_evaluations = Optimization.total_time
+                
                 
                 #The file path for current optimizer.
                 config_per_group_directory=parse_directory([config_per_optimizer_directory,opt])
                 
-                
-                if save == True:
-                    try:
-                        Path(score_per_optimizer_directory).mkdir(parents=True, exist_ok=True)
-                        Path(surrogate_time_per_optimizer_directory).mkdir(parents=True, exist_ok=True)
-                        Path(objective_time_per_optimizer_directory).mkdir(parents=True, exist_ok=True)
-                        Path(acquisition_time_per_optimizer_directory).mkdir(parents=True, exist_ok=True)
-                        Path(total_time_per_optimizer_directory).mkdir(parents=True, exist_ok=True)
-                        Path(config_per_group_directory).mkdir(parents=True, exist_ok=True)
-                    except FileExistsError:
-                        print("Folder is already there")
-                        
-                    else:
-                        print("Folder is created there")
-                        
-                    """pd.DataFrame(y_evaluations).to_csv( parse_directory([ score_per_optimizer_directory, opt+csv_postfix ]))
-                    pd.DataFrame(surrogate_time_evaluations).to_csv( parse_directory([ surrogate_time_per_optimizer_directory, opt+csv_postfix ]))
-                    pd.DataFrame(objective_time_evaluations).to_csv( parse_directory([ objective_time_per_optimizer_directory, opt+csv_postfix ]))
-                    pd.DataFrame(acquisition_time_evaluations).to_csv( parse_directory([ acquisition_time_per_optimizer_directory, opt+csv_postfix ]))
-                    pd.DataFrame(total_time_evaluations).to_csv( parse_directory([ total_time_per_optimizer_directory, opt+csv_postfix ]))
-                    """
-
-
-
-
-
-
 
 
 def get_openml_data(speed = None):
@@ -187,11 +154,13 @@ def get_openml_data(speed = None):
         return [14954,11,3918,3917,3021,43,167141,9952]
     return [2074,9976,9910,167125]
     
+    
 
+#
 def get_jad_data(speed = None):
     assert speed !=None
     if speed == 'fast':
-        return [839, 847,1114] #842,851,850
+        return [839, 847,1114,842,851,850] #
     #  on all seeds 
     return [843,883,866]
     
@@ -199,14 +168,14 @@ def get_jad_data(speed = None):
 if __name__ == '__main__':
     config_of_data = { 'Jad':{'data_ids':get_jad_data},
                         'OpenML': {'data_ids':get_openml_data}      }
-    opt_list = ['SMAC' ] # ,,'Random_Search','RF_Local',]
+    opt_list = ['Multi_RF_Local'] # ,'Multi_RF_Local' ,'Random_Search','RF_Local',]
     for speed in ['fast']:
      # obtain the benchmark suite    
-        for repo in ['Jad','OpenML']:
+        for repo in ['OpenML','Jad']: #,'Jad' ,
             #XGBoost Benchmark    
             xgb_bench_config =  {
                 'n_init' : 10,
-                'max_evals' : 50,
+                'max_evals' : 550,
                 'n_datasets' : 1000,
                 'data_ids' :  config_of_data[repo]['data_ids'](speed=speed),
                 'n_seeds' : [1,2,3], #
@@ -216,3 +185,9 @@ if __name__ == '__main__':
                 'data_repo' : repo
             }
             run_benchmark_total(opt_list,xgb_bench_config)
+
+    
+
+
+
+    
