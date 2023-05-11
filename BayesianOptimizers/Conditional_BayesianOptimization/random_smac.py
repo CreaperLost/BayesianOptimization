5a15feea9d0c5e61a1323022bf4e6ae8e32dcecd
CreaperLost <creaperlost@gmail.com>
2023-05-11 16:02:04 +0300

    new changes

--- BayesianOptimizers/Conditional_BayesianOptimization/random_smac.py
+++ BayesianOptimizers/Conditional_BayesianOptimization/random_smac.py
@@ -3,15 +3,15 @@ from smac import HyperparameterOptimizationFacade, Scenario
 from ConfigSpace import Configuration, ConfigurationSpace
 import pandas as pd
 import numpy as np
+from smac.facade.random_facade import RandomFacade
 
-
-class SMAC_HPO:
+class Random_SMAC:
                 
 
     def __init__(self,configspace,config_dict,task_id,repo,max_evals,seed,objective_function,n_workers=1):
         #Scenario object specifying the optimization environment
         self.scenario = Scenario(configspace,name='Dataset'+str(task_id),
-                                           output_directory='single_smac' +str(n_workers) +'/'+repo,
+                                           output_directory='SIngle_smac_random' +str(n_workers) +'/'+repo,
                                      n_trials=max_evals,deterministic=True,seed=seed,n_workers = n_workers )
          
         # Use SMAC to find the best configuration/hyperparameters
@@ -24,7 +24,7 @@ class SMAC_HPO:
         
         self.fX = np.array([])
         self.X_group = np.array([])
-        
+        self.total_time = np.array([])
 
         self.acquisition_time = np.array([0 for i in range(max_evals)])
         self.surrogate_time = np.array([0 for i in range(max_evals)])
@@ -33,25 +33,9 @@ class SMAC_HPO:
         self.inc_score = np.inf
         self.inc_config = {}
 
-        self.total_time = pd.DataFrame(columns=['Time','Score'])  
-
-    def load(self):
-        smac_single = HyperparameterOptimizationFacade(self.scenario, self.objective_function,overwrite=False)
-        for item in smac_single.intensifier.trajectory:
-            # Single-objective optimization
-            assert len(item.config_ids) == 1
-            assert len(item.costs) == 1
-
-            y = item.costs[0]
-            x = item.walltime
-
-            new_row = pd.DataFrame({'Time':x,'Score':y},index=[0])
-            self.total_time = self.total_time.append(new_row,ignore_index=True)
-        print(self.total_time)
-
     def run(self):
 
-        smac_single = HyperparameterOptimizationFacade(self.scenario, self.objective_function,overwrite=True)
+        smac_single = RandomFacade(self.scenario, self.objective_function,overwrite=True)
         incumbent_single = smac_single.optimize()
         
         # Plot all trials
@@ -62,17 +46,16 @@ class SMAC_HPO:
                 
             # Trial value
             cost = trial_value.cost
-            #time = trial_value.time
+            time = trial_value.time
 
-            #if cost < self.inc_score:
-            #    self.inc_config  = config_descr.get_dictionary()
-            #    self.inc_score = cost
-            #    print(f'Best config {self.inc_config} at step: {trial_info.config_id}, score :{cost}')
+            if cost < self.inc_score:
+                self.inc_config  = config_descr.get_dictionary()
+                self.inc_score = cost
+                print(f'Best config {self.inc_config} at step: {trial_info.config_id}, score :{cost}')
 
             #add time and fX.
             self.fX = np.append(self.fX,np.array(cost))
-            
-            #self.total_time = np.append(self.total_time,np.array(time))   
+            self.total_time = np.append(self.total_time,np.array(time))   
 
             self.X_group = np.append(self.X_group,config_descr['model']) 
             #This is a better interpretable form of storing the configurations.
@@ -83,20 +66,6 @@ class SMAC_HPO:
             y_df = pd.DataFrame({'y':cost},index=[0])
             new_row = pd.concat([X_df,y_df],axis=1)
             self.save_configuration[config_descr['model']] = self.save_configuration[config_descr['model']].append(new_row,ignore_index=True)
-
-
-        
-        for item in smac_single.intensifier.trajectory:
-            # Single-objective optimization
-            assert len(item.config_ids) == 1
-            assert len(item.costs) == 1
-
-            y = item.costs[0]
-            x = item.walltime
-            new_row = pd.DataFrame({'Time':x,'Score':y},index=[0])
-            self.total_time = self.total_time.append(new_row,ignore_index=True)
-        
-        print(self.total_time)
-
+            
 
             
